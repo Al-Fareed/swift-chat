bf15610e537bbadb99a396e1d0ff5a6b6c1b7df2
Al-Fareed <alfareedss472>
2026-02-09 18:23:14 +0530

    Initial Commit

---
+++ Swift Chat/Swift_ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_ChatApp.swift
+//  Swift Chat
+//
+//  Created by Al-Fareed on 09/02/26.
+//
+
+import SwiftUI
+
+@main
+struct Swift_ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
